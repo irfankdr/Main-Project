7ac01f630bede6d3143effeeb583e6a1554b828b
Irfan <irfanvk96@gmail.com>
2025-03-03 21:28:51 +0530

    second

--- FestApp/urls.py
+++ FestApp/urls.py
@@ -8,5 +8,16 @@ urlpatterns = [
     path('clgregister',views.clgregister),
     path('judge_home',views.judge_home),
     path('logincode',views.logincode),
-    path('staff_home',views.staff_home),
+
+    path('eventorg_home',views.create_event),
+    path('eventorg_home',views.create_program),
+    path('eventreg',views.eventreg),
+    path('clgregisterpost',views.clgregisterpost),
+    path('verifyeventco',views.verifyeventco),
+    path('event_co_home',views.event_co_home),
+    path('manageevent',views.manageevent),
+    path('addevent',views.addevent),
+    path('addevent_post',views.addevent_post),
+
+
 ]
--- FestHub/urls.py
+++ FestHub/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('FestApp.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
